a3a574ee51aa7d1ffc58ebc326ed81c985b2be46
FlorentCmb <AS0D@protonmail.ch>
2021-04-29 13:33:15 +0200

    Clean App files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// Component
+import EmailInput from './components/EmailInput'
+// Style
+import './App.css'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
