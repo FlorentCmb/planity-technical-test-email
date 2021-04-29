92f4c5fa4beea9cab19182683f26ffad06fd6991
FlorentCmb <AS0D@protonmail.ch>
2021-04-29 16:30:33 +0200

    Add the fill function in the component

--- src/components/EmailInput.js
+++ src/components/EmailInput.js
@@ -1,11 +1,14 @@
 // Librairies
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 // JSON import
 import JsonProviders from '../assets/providers.json'
 // Style
 import './EmailInput.css'
 
 const EmailInput = () => {
+
+    /* Ref */
+    const inputRef = useRef(null)
 
     /* States */
     const [emailValue, setEmailValue] = useState('')
@@ -17,11 +20,12 @@ const EmailInput = () => {
     useEffect(() => {
         setProviders(JsonProviders)
         setPopularSuggestions(["gmail.com", "outlook.com", "yahoo.fr"])
+        inputRef.current.focus()
     }, [])
 
     useEffect(() => {
         if (emailValue.includes('@') && !providers.includes(emailValue.split('@')[1])) getSuggestions(emailValue)
-        else if (emailValue.length > 0) setSuggestions(popularSuggestions)
+        else if (emailValue.length > 0 && !providers.includes(emailValue.split('@')[1])) setSuggestions(popularSuggestions)
         else setSuggestions([])
     }, [emailValue])
     
@@ -41,7 +45,6 @@ const EmailInput = () => {
             let otherMatches = []
             // We push the entries with the lowest index in the best matches
             for (let i = 0; i < potentialSuggestions.length; i++) {
-                console.log(potentialSuggestions[i].indexOf(splittedValue))
                 if (potentialSuggestions[i].indexOf(splittedValue) === 0) bestMatches.push(potentialSuggestions[i])
                 else if (potentialSuggestions[i].indexOf(splittedValue) > 0) otherMatches.push(potentialSuggestions[i])
             }
@@ -50,17 +53,23 @@ const EmailInput = () => {
             const bestSuggestions = bestMatches.sort().concat(otherMatches.sort())
             setSuggestions(bestSuggestions.slice(0, 3))
         }
-        else {
-            setSuggestions(popularSuggestions)
-        }
+    }
+
+    // The function which will fill the input if we click on a suggestion
+    const fillInputWithSuggestion = suggestion => {
+        // This will fill the input with the suggestion and the @ if it's not already written
+        const finalValue = emailValue.includes('@') ? emailValue.slice(0, emailValue.indexOf('@') + 1) + suggestion : emailValue + '@' + suggestion
+        setEmailValue(finalValue)
+        // This will give the focus back to the input
+        inputRef.current.focus()
     }
 
     return (
         <div className="EmailInput">
             <label className="EmailInput-Label">Email :</label>
-            <input id="EmailInput-Input" name="email" type="email" placeholder="Ex : jean.dupont@gmail.com" value={emailValue} onChange={inputHandler} />
-            <ul>{suggestions.length > 0 && !providers.includes(emailValue) ? suggestions.map((suggestion, index) => (
-                <li key={index}>{suggestion}</li>
+            <input id="EmailInput-Input" name="email" type="email" placeholder="Ex : jean.dupont@gmail.com" value={emailValue} onChange={inputHandler} ref={inputRef} />
+            <ul className="EmailInput-List">{suggestions.length > 0 ? suggestions.map((suggestion, index) => (
+                <li className="EmailInput-List-Item" onClick={() => fillInputWithSuggestion(suggestion)} key={index}>{suggestion}</li>
             )) : ""}
             </ul>
         </div>
